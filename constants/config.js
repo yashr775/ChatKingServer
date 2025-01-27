648f2bfc2c029dc3ff110d27c93acfcaabcf017b
yashr775 <rajy29629@gmail.com>
2025-01-27 22:34:12 +0530

    Some bugs fixed

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,6 @@
 const corsOptions = {
     origin: ["http://localhost:5173", "http://localhost:4173", process.env.CLIENT_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 
 }
