66b4e42e6af90b795b23e41f95af6514bbd5c083
yashr775 <rajy29629@gmail.com>
2025-01-27 19:00:22 +0530

    Port updated

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ io.on("connection", (socket) => {
 
 app.use(errorMiddleware);
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log(`App is listening on ${PORT} in ${envMode}`);
 });
 
