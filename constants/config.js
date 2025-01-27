39fc82a8e05c032ea8dbacea90f2ec41bc9bc8d2
yashr775 <rajy29629@gmail.com>
2025-01-27 23:00:26 +0530

    Bugs fixed

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:4173", process.env.CLIENT_URL],
+    origin: ["http://localhost:5173", "http://localhost:4173", "https://chatkingdep-3yxkyg402-yashs-projects-bc7edce6.vercel.app", process.env.CLIENT_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 
